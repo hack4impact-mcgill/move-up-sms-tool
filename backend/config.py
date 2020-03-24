4399d2e123f897d0ca2597ef9623f063f8ce7147
cc-21 <55864124+cc-21@users.noreply.github.com>
2020-03-24 00:09:48 -0400

    #35 Modify the config file

--- backend/config.py
+++ backend/config.py
@@ -1,25 +1,23 @@
 import os
 
 class Config:
-    APP_NAME = "Move Up CRM Tool"
+    APP_NAME = "Move Up Sign-Up Tool"
     SECRET_KEY = os.environ.get("SECRET_KEY") or "hard to guess string"
+    DATABASE_URL = str(os.environ.get("DEV_URL"))
 
     @staticmethod
     def init_app(app):
         pass
 
-
 class DevelopmentConfig(Config):
     DEBUG = True
-
 
 class TestingConfig(Config):
     TESTING = True
 
-
 class ProductionConfig(Config):
     PRODUCTION = True
-
+    DATABASE_URL = str(os.environ.get("PROD_URL"))
 
 config = {
     "development": DevelopmentConfig,
