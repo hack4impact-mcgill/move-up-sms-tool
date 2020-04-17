7e617f4446590ae374d107dfb1a147dbf040707d
Xinrui Li <xinruili@hotmail.com>
2020-04-17 00:29:30 -0400

    Fetching the questions from a different airtable table

--- backend/app/main/__init__.py
+++ backend/app/main/__init__.py
@@ -1,10 +1,36 @@
+import os
+import requests
+import json
 from flask import Blueprint
 from .parsers import survey_from_json
 
 main = Blueprint('main', __name__) 
 
-with open('signup_form.json') as survey_file:
-     signup_survey = survey_from_json(survey_file.read())
+questions = requests.get( "https://api.airtable.com/v0/appZa3BCfY1eRJCzU/Table%201",
+            headers={"Authorization": str(os.environ.get("AIRTABLE_XIN_TEST_KEY"))})
+
+if (questions.status_code == 200): 
+     questions_json = questions.json()
+     new_json_form = {
+     "questions": [
+          {
+          "text": questions_json["records"][0]["fields"]["Name question"],
+          "kind": "text",
+          "airtable_id": "Name"
+          },
+          {
+          "text": questions_json["records"][0]["fields"]["Email question"],
+          "kind": "email",
+          "airtable_id": "Email"
+          }
+     ],
+     "title": "sign-up form"
+     }
+     signup_survey = survey_from_json(json.dumps(new_json_form))
+
+else:
+     with open('signup_form.json') as survey_file:
+          signup_survey = survey_from_json(survey_file.read())
 
 from . import views, question_views, answer_views, survey_views
 
