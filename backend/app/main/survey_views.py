5f3584910727b4a2a4b8448c96ee951da4d84433
Thomas Wright <thomasgudjonwright@gmail.com>
2020-05-12 20:13:44 -0400

    Fixing double elif

--- backend/app/main/survey_views.py
+++ backend/app/main/survey_views.py
@@ -27,7 +27,6 @@ def sms_signup():
         response.redirect(url=answer_url)
     elif body == 'MOVEUP':
         # First contact from user, send our welcome message
-            elif body == 'MOVEUP':
         welcome_user(response.message, (retrieve_prev_record(phone_number)=="NONE"))
     elif body == 'SIGNUP':
         # User proceeds from welcome and begins signup process
